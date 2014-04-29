e83faeafc8bd7bb59fa8ab86b589cc6f83bd2201
Yasyf Mohamedali <yasyfm@gmail.com>
2014-04-29 16:39:59 -0400

    web Procfile

--- web.sh
+++ web.sh
@@ -1,10 +1,6 @@
-if [ "$is_worker" == "True" ]; then
-	python monitor.py
+if [ "$dev" == "True" ]; then
+	python textbooks.py
 else
-	if [ "$dev" == "True" ]; then
-		python textbooks.py
-	else
-		echo "$cert" > cert.pem
-		gunicorn -b "0.0.0.0:$PORT" textbooks:app
-	fi
+	echo "$cert" > cert.pem
+	gunicorn -b "0.0.0.0:$PORT" textbooks:app
 fi
