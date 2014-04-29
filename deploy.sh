6a3247405d824c859ec72dc643eba8d9da948eb0
Yasyf Mohamedali <yasyfm@gmail.com>
2014-04-29 16:49:52 -0400

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-git checkout web_procfile
+echo "Deploying Web"
+git checkout -b web_procfile
+echo "web: ./web.sh" > Procfile
 git push heroku web_procfile:master
+git checkout master
 
-git checkout worker_procfile
+echo "Deploying Workers"
+git checkout -b worker_procfile
+echo "worker: ./worker.sh" > Procfile
 git push worker-1 worker_procfile:master
 git push worker-2 worker_procfile:master
 git push worker-3 worker_procfile:master
-
 git checkout master
+
+git branch -D web_procfile
+git branch -D worker_procfile
