d541034260193b7fe49d7630c95e13fab0eca0f5
Yasyf Mohamedali <yasyfm@gmail.com>
2014-04-29 16:54:50 -0400

    commit before deploying

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,19 @@
 echo "Deploying Web"
 git checkout -b web_procfile
 echo "web: ./web.sh" > Procfile
-git push heroku web_procfile:master
+git add Procfile
+git commit -m 'web Procfile'
+git push heroku web_procfile:master --force
 git checkout master
 
 echo "Deploying Workers"
 git checkout -b worker_procfile
 echo "worker: ./worker.sh" > Procfile
-git push worker-1 worker_procfile:master
-git push worker-2 worker_procfile:master
-git push worker-3 worker_procfile:master
+git add Procfile
+git commit -m 'worker Procfile'
+git push worker-1 worker_procfile:master --force
+git push worker-2 worker_procfile:master --force
+git push worker-3 worker_procfile:master --force
 git checkout master
 
 git branch -D web_procfile
