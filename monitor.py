d854c84bd3ec35e9a50d27271b519b09b4a5fb08
Yasyf Mohamedali <yasyfm@gmail.com>
2014-05-04 06:10:31 -0400

    workers warn on fail

--- monitor.py
+++ monitor.py
@@ -34,23 +34,39 @@ def error_mail():
 		print 'Stuck Worker'
 		print repr(task)
 
-while True:
-	last_task = task
-	task = queue.find_one(sort=[("time", 1)])
-	if task and last_task and task == last_task and not mailed:
-		mailed = True
-		error_mail()
-	if task:
-		queue.remove(task)
-		_id = task['class_id']
-		print 'Processing {_id}'.format(_id=_id)
-		if task['group']:
-			group_obj = get_group(_id)
-			for c in group_obj.class_ids:
-				do_get_class(c)
-		elif task['update']:
-			update_textbooks(_id)
+def fail_mail(e):
+	message = sendgrid.Mail()
+	message.add_to(os.getenv('admin_email'))
+	message.set_subject('Crashing Worker @ MIT Textbooks')
+	trace = traceback.format_exc() 
+	message.set_html(request.url + '<br><br>' + e.message + '<br><br><pre>' + trace + '</pre>')
+	message.set_text(request.url + '\n\n' + e.message + '\n\n' + trace)
+	message.set_from('MIT Textbooks <tb_support@mit.edu>')
+	try:
+		sg.send(message)
+	except Exception:
+		pass
+
+try:
+	while True:
+		last_task = task
+		task = queue.find_one(sort=[("time", 1)])
+		if task and last_task and task == last_task and not mailed:
+			mailed = True
+			error_mail()
+		if task:
+			queue.remove(task)
+			_id = task['class_id']
+			print 'Processing {_id}'.format(_id=_id)
+			if task['group']:
+				group_obj = get_group(_id)
+				for c in group_obj.class_ids:
+					do_get_class(c)
+			elif task['update']:
+				update_textbooks(_id)
+			else:
+				do_get_class(_id)
 		else:
-			do_get_class(_id)
-	else:
-		time.sleep(5)
+			time.sleep(5)
+except Exception, e:
+	fail_mail(e)
