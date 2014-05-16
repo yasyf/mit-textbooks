52313a524ccac296d4b624e9ab55c970b1cf9e2a
Yasyf Mohamedali <yasyfm@gmail.com>
2014-05-16 19:59:43 -0400

    added newrelic, bugsnag

--- dev.sh
+++ dev.sh
@@ -2,7 +2,9 @@ if [ "$is_worker" == "True" ]; then
 	python monitor.py
 else
 	if [ "$dev" == "True" ]; then
+		memcached -d
 		python textbooks.py
+		killall memcached
 	else
 		echo "$cert" > cert.pem
 		gunicorn -b "0.0.0.0:$PORT" textbooks:app
